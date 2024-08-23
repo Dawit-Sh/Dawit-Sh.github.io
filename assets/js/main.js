1660eb81d3ff75465d529a4c9f36dfa9b4233996
nk <nk@localhost.localdomain>
2024-08-23 09:25:23 -0400

    major update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,47 +1,60 @@
-const telegramBtn = document.getElementById("telegram-btn");
-const githubBtn = document.getElementById("github-btn");
-const linkedinBtn = document.getElementById("linkedin-btn");
-const blogBtn = document.getElementById("blog-btn");
-const bookshelfBtn = document.getElementById("bookshelf-btn");
+const yearElement = document.getElementById("year");
+const aliveTimeElement = document.getElementById("aliveTime");
+const copyrightElement = document.getElementById("copyright");
+const percentElapsedElement = document.getElementById("percentElapsed");
+const birthDate = new Date("2004-05-27");
+const lifeExpectancy = 66; // years
 
-const project1Btn = document.getElementById("project1-btn");
-const project2Btn = document.getElementById("project2-btn");
+function updateYear() {
+  const currentDate = new Date();
+  const currentYear = currentDate.getFullYear();
+  const isBirthday =
+    currentDate.getMonth() === 4 && currentDate.getDate() === 27; // May is 4 (0-indexed)
 
-const telegramPopup = document.getElementById("telegram-popup");
-const githubPopup = document.getElementById("github-popup");
-const linkedinPopup = document.getElementById("linkedin-popup");
-const blogPopup = document.getElementById("blog-popup");
-const bookshelfPopup = document.getElementById("bookshelf-popup");
-
-const project1Popup = document.getElementById("project1-popup");
-const project2Popup = document.getElementById("project2-popup");
-
-const overlay = document.querySelector(".overlay");
-const popupCloseBtns = document.querySelectorAll(".popup-close");
-
-telegramBtn.addEventListener("click", () => openPopup(telegramPopup));
-githubBtn.addEventListener("click", () => openPopup(githubPopup));
-linkedinBtn.addEventListener("click", () => openPopup(linkedinPopup));
-blogBtn.addEventListener("click", () => openPopup(blogPopup));
-bookshelfBtn.addEventListener("click", () => openPopup(bookshelfPopup));
-
-project1Btn.addEventListener("click", () => openPopup(project1Popup));
-project2Btn.addEventListener("click", () => openPopup(project2Popup));
-
-overlay.addEventListener("click", closeAllPopups);
-
-popupCloseBtns.forEach((btn) => {
-  btn.addEventListener("click", closeAllPopups);
-});
-
-function openPopup(popup) {
-  popup.classList.add("active");
-  overlay.classList.add("active");
+  if (isBirthday) {
+    const age = currentYear - birthDate.getFullYear();
+    yearElement.textContent = `'${age.toString().slice(-2)}`;
+  }
 }
 
-function closeAllPopups() {
-  document
-    .querySelectorAll(".popup")
-    .forEach((popup) => popup.classList.remove("active"));
-  overlay.classList.remove("active");
+function calculateAliveTime() {
+  const now = new Date();
+  const timeDiff = now - birthDate;
+
+  const days = Math.floor(timeDiff / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(
+    (timeDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const minutes = Math.floor((timeDiff % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((timeDiff % (1000 * 60)) / 1000);
+
+  return `${days}d ${hours}h ${minutes}m ${seconds}s`;
 }
+
+function calculatePercentElapsed() {
+  const now = new Date();
+  const timeDiff = now - birthDate;
+  const yearsElapsed = timeDiff / (1000 * 60 * 60 * 24 * 365.25);
+  const percentElapsed = (yearsElapsed / lifeExpectancy) * 100;
+  return percentElapsed.toFixed(6);
+}
+
+function updateAliveTime() {
+  aliveTimeElement.textContent = calculateAliveTime();
+  percentElapsedElement.textContent = `${calculatePercentElapsed()}%`;
+}
+
+function updateCopyright() {
+  const currentYear = new Date().getFullYear();
+  const repoUrl = "https://github.com/Dawit-Sh/Dawit-Sh.github.io"; 
+  copyrightElement.innerHTML = `©${currentYear} Dawit Sharon. <a href="${repoUrl}" target="_blank" style="text-decoration: none; color: inherit;">Code it, remix it, and pass it on!</a>`;
+}
+
+
+// Update year, copyright, and set interval to check daily
+updateYear();
+updateCopyright();
+setInterval(updateYear, 24 * 60 * 60 * 1000);
+
+// Update alive time and percent elapsed every second
+setInterval(updateAliveTime, 1000);
